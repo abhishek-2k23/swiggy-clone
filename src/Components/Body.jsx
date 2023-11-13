75b9b925c1fb344c27fc00fcbee60b8dd206d8be
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-11-13 21:58:15 +0530

    routing config done

---
+++ src/Components/Body.jsx
@@ -0,0 +1,8 @@
+const Body = () =>{
+    return (
+        <div>
+            <h1>body</h1>
+        </div>
+    )
+}
+export default Body;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () =>{
+    return (
+        <div>
+            <h1 className="text-lg">Footer</h1>
+        </div>
+    )
+}
+export default Footer;
