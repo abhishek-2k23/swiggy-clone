6e66391d22dde7988b96281cbcd8cff4ac5d49db
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-11-13 22:56:55 +0530

    constant file

---
+++ src/constant.js
@@ -0,0 +1,279 @@
+export const restaurants = [
+    {
+    "info": {
+    "id": "221504",
+    "name": "Well Food",
+    "cloudinaryImageId": "dpaj42ojdef7ntgv3hlo",
+    "locality": "Tempo Stand",
+    "areaName": "Kankarbagh",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Indian",
+    "Chinese",
+    "South Indian",
+    "Biryani"
+    ],
+    "avgRating": 3.5,
+    "feeDetails": {
+    "restaurantId": "221504",
+    "fees": [
+    {
+    "name": "BASE_DISTANCE",
+    "fee": 2300
+    },
+    {
+    "name": "BASE_TIME"
+    },
+    {
+    "name": "ANCILLARY_SURGE_FEE"
+    }
+    ],
+    "totalFee": 2300
+    },
+    "parentId": "225606",
+    "avgRatingString": "3.5",
+    "totalRatingsString": "10K+",
+    "promoted": true,
+    "adTrackingId": "cid=9184217~p=1~eid=0000018b-c99f-98c4-5dc6-981e005a0175~srvts=1699894761668~45826",
+    "sla": {
+    "deliveryTime": 21,
+    "lastMileTravel": 1.4,
+    "serviceability": "SERVICEABLE",
+    "slaString": "21 mins",
+    "lastMileTravelString": "1.4 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-11-14 02:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {
+    "header": "₹125 OFF",
+    "shortDescriptionList": [
+    {
+    "meta": "Use SUPERSAVER",
+    "discountType": "Flat",
+    "operationType": "RESTAURANT"
+    }
+    ],
+    "descriptionList": [
+    {
+    "meta": "Flat ₹125 off on orders above ₹249 | Use code SUPERSAVER",
+    "discountType": "Flat",
+    "operationType": "RESTAURANT"
+    }
+    ]
+    },
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "textBased": {},
+    "imageBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "swiggy://menu?restaurant_id=221504",
+    "text": "RESTAURANT_MENU",
+    "type": "DEEPLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "591085",
+    "name": "0612 Road Chef",
+    "cloudinaryImageId": "qo1ypqreeaulytd7cmgr",
+    "locality": "Sanjay Nagar Road",
+    "areaName": "Kankarbagh",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "Indian",
+    "Tandoor",
+    "Chinese",
+    "Biryani"
+    ],
+    "avgRating": 4.1,
+    "feeDetails": {
+    "restaurantId": "591085",
+    "fees": [
+    {
+    "name": "BASE_DISTANCE",
+    "fee": 2300
+    },
+    {
+    "name": "BASE_TIME"
+    },
+    {
+    "name": "ANCILLARY_SURGE_FEE"
+    }
+    ],
+    "totalFee": 2300
+    },
+    "parentId": "22680",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 22,
+    "lastMileTravel": 1.3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "22 mins",
+    "lastMileTravelString": "1.3 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-11-14 00:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {
+    "header": "₹150 OFF",
+    "shortDescriptionList": [
+    {
+    "meta": "Use MDM150",
+    "discountType": "Flat",
+    "operationType": "RESTAURANT"
+    }
+    ],
+    "descriptionList": [
+    {
+    "meta": "Flat ₹150 off  | Use code MDM150",
+    "discountType": "Flat",
+    "operationType": "RESTAURANT"
+    }
+    ]
+    },
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "textBased": {},
+    "imageBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "swiggy://menu?restaurant_id=591085",
+    "text": "RESTAURANT_MENU",
+    "type": "DEEPLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "242395",
+    "name": "Ram Babu Restaurant",
+    "cloudinaryImageId": "mq1vlwbzjanzdeukja3d",
+    "locality": "Chandmadi road",
+    "areaName": "Kankarbagh",
+    "costForTwo": "₹297 for two",
+    "cuisines": [
+    "Chinese",
+    "North Indian",
+    "Biryani"
+    ],
+    "avgRating": 3.5,
+    "feeDetails": {
+    "restaurantId": "242395",
+    "fees": [
+    {
+    "name": "BASE_DISTANCE",
+    "fee": 2300
+    },
+    {
+    "name": "BASE_TIME"
+    },
+    {
+    "name": "ANCILLARY_SURGE_FEE"
+    }
+    ],
+    "totalFee": 2300
+    },
+    "parentId": "166239",
+    "avgRatingString": "3.5",
+    "totalRatingsString": "10K+",
+    "sla": {
+    "deliveryTime": 21,
+    "lastMileTravel": 0.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "21 mins",
+    "lastMileTravelString": "0.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-11-13 23:30:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {
+    "header": "₹150 OFF",
+    "shortDescriptionList": [
+    {
+    "meta": "Use WCHOTSTAR",
+    "discountType": "Flat",
+    "operationType": "RESTAURANT"
+    }
+    ],
+    "descriptionList": [
+    {
+    "meta": "FLAT ₹150 off on order above ₹349 | Use MATCHDAYTREAT",
+    "discountType": "Flat",
+    "operationType": "RESTAURANT"
+    }
+    ]
+    },
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "textExtendedBadges": {},
+    "textBased": {},
+    "imageBased": {}
+    }
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "swiggy://menu?restaurant_id=242395",
+    "text": "RESTAURANT_MENU",
+    "type": "DEEPLINK"
+    }
+    },
+]
