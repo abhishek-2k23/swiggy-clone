211b02ce311eafdd9f5e1ef50080fc0400a29c45
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-11-14 13:41:47 +0530

    api call done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,7 +1,36 @@
+import RestaurentCard from "./RestaurentCard.jsx";
+import {restaurants} from "../constant.js";
+import { API } from "../constant.js";
+import { useState,useEffect } from "react";
+
 const Body = () =>{
+    const [restaurantData, setRestaurantData] = useState(null);
+
+    //calling the api using useEffect
+    useEffect(() =>{
+        getRestaurants();
+    },[])
+
+    //api calling function
+    async function getRestaurants(){
+        const res = await fetch(API);
+        const data = await res.json();
+        console.log("data : ",data);
+        setRestaurantData(data?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        console.log("Restaurants : ",restaurantData)
+    }
+
     return (
-        <div>
-            <h1>body</h1>
+        <div className="w-full flex  justify-center">
+            <div className="w-10/12 h-auto border border-green-300 ">
+                <h1 className="font-bold text-lg">Restaurants : </h1>
+                <div className="flex gap-1 w-full h-auto flex-wrap justify-evenly ">
+                {restaurantData?.map((restaurant) => {
+                    return <RestaurentCard key={restaurant?.info?.id} restaurant = {restaurant?.info}/>
+                })}
+                {/* {console.log(restaurantData)} */}
+                </div>
+            </div>
         </div>
     )
 }
