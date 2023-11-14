9fd73f9241a19deac546cbdd9ea312528ea6a54c
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-11-14 19:29:53 +0530

    bg-gradient and navigation to single restaurant

--- src/Components/RestaurentCard.jsx
+++ src/Components/RestaurentCard.jsx
@@ -1,19 +1,20 @@
 import { ImgUrl } from "../constant";
 import { MdStars } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 const RestaurentCard = (props) =>{
     const {restaurant} = props;
+    const navigate = useNavigate();
     return ( 
-        <div className="m-2 w-[249px] h-[269px] hover:scale-95 transition-all duration-50">
+        <div className="m-2 w-[249px] h-[269px] hover:scale-95 transition-all duration-50" onClick={() => navigate(`/restaurant/${restaurant?.id}`)}>
             <div className=" rounded-md w-[249px] h-[166px] relative shadow-inner">
                 <img src={ImgUrl + restaurant.cloudinaryImageId} alt='backgroundImage' className="w-[249px] h-[166px] rounded-lg"/>
-                <div className="absolute h-12 w-full bg- bottom-0 rounded-b-md">
-
+                <div className="absolute h-14 w-full bg-gradient-to-t from-black bottom-0 rounded-b-md "> 
+                    <p className="absolute text-white font-extrabold bottom-2 text-xl left-1">{`${restaurant?.aggregatedDiscountInfoV3.header} ${restaurant?.aggregatedDiscountInfoV3.subHeader}`}</p>
                 </div>
-                <p className="absolute text-white font-extrabold bottom-2 text-xl left-1">{`${restaurant?.aggregatedDiscountInfoV3.header} ${restaurant?.aggregatedDiscountInfoV3.subHeader}`}</p>
             </div>
             <div className="flex flex-col justify-start text-gray-800">
-                <h1 className="font-bold ">{restaurant.name.split("50")}</h1>
-                {console.log(restaurant.name.split(0,40))}
+                <h1 className="font-bold ">{restaurant.name.substring(0,33)}</h1>
+                {console.log()}
                 <div className="flex items-center gap-1 font-bold" >
                     <MdStars className="text-green-700 text-lg"/>
                     <h1 className="flex justify-center items-center gap-2">{`${restaurant?.avgRatingString}`}</h1> 
