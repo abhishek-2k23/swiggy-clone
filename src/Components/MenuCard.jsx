06e3f08a3d586a3f6ff20f1c0a1f4d9f764b73ef
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-11-16 08:59:10 +0530

    bestseller mark added

--- src/Components/MenuCard.jsx
+++ src/Components/MenuCard.jsx
@@ -1,22 +1,34 @@
 import { PiSquareLogoFill } from "react-icons/pi";
 import { MdOutlineCurrencyRupee } from "react-icons/md";
 import { MenuImgUrl } from "../constant";
+import { IoStar } from "react-icons/io5";
 
 const MenuCard = ({ menu }) => {
   return (
-    <div className="text-lg font-semibold flex justify-between items-center gap-1 py-2 border-b">
+    <div className="text-lg font-semibold flex justify-between items-center gap-1 pt-5 pb-10 border-b">
       <div className="flex flex-col gap-2">
-        {menu?.card?.info?.itemAttribute?.vegClassifier === "VEG" ? (
-          <PiSquareLogoFill className="text-green-500 text-base " />
-        ) : (
-          <PiSquareLogoFill className="text-red-500 text-lg font-bold" />
-        )}
+        <div className="flex gap-2 items-center">
+          <span>
+            {menu?.card?.info?.itemAttribute?.vegClassifier === "VEG" ? (
+              <PiSquareLogoFill className="text-green-500 text-base " />
+              ) : (
+              <PiSquareLogoFill className="text-red-500 text-lg font-bold" />
+            )}
+
+          </span>
+          {menu?.card?.info?.isBestseller && <span className="flex gap-2 items-center text-base text-orange-500">
+            <IoStar/>
+            <p>Bestseller</p>
+          </span>}
+                
+        </div>
+        
 
         <p className="text-base">{menu?.card?.info?.name} </p>
 
         <p className="flex justify-start items-center text-base">
           <MdOutlineCurrencyRupee />
-          {menu?.card?.info?.price}
+          {menu?.card?.info?.price?.toString().slice(0, -2)}
         </p>
         <p className="text-gray-400 font-semibold text-sm w-11/12">
           {menu?.card?.info?.description}
@@ -25,19 +37,17 @@ const MenuCard = ({ menu }) => {
       <div>
         {menu?.card?.info?.imageId ? (
           <div className="relative">
-            <div 
-              className="w-[118px] h-[96px] rounded-md">
-                <img
-              src={MenuImgUrl + menu?.card.info.imageId}
-              alt="" className="w-full h-full rounded-md"
-                />
-                
-                <p className="absolute -bottom-2  px-8 py-1 left-3 text-sm rounded-md bg-white border border-gray-300 drop-shadow-sm hover:drop-shadow-lg text-green-500 cursor-pointer tracking-wider">
-                    ADD
-                </p>
+            <div className="w-[118px] h-[96px] rounded-md">
+              <img
+                src={MenuImgUrl + menu?.card.info.imageId}
+                alt=""
+                className="w-full h-full rounded-md"
+              />
+
+              <p className="absolute -bottom-3  px-8 py-2 left-3 text-sm rounded-md bg-white border border-gray-300 drop-shadow-sm hover:drop-shadow-lg text-green-500 cursor-pointer tracking-wider">
+                ADD
+              </p>
             </div>
-            
-            
           </div>
         ) : (
           <div className="w-28 h-10 border border-gray-400 hover:drop-shadow-md drop-shadow-sm text-center rounded-md flex justify-center items-center">
