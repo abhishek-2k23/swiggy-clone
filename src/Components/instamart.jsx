1fb791a6747645be48ce6b8cdbb8e6ffbfe7a8e3
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-11-17 16:59:20 +0530

    auto collaplse menu

---
+++ src/Components/instamart.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const Section = ({ title, desc, isVisible, setIsVisible }) => {
+  return (
+    <div className="border border-black p-4 m-4">
+      <h1 className="font-bold text-lg">{title}</h1>
+      {isVisible === title ? (
+        <div>
+          {" "}
+          <p onClick={() => setIsVisible(" ")}>Hide</p>
+          <p>{desc}</p>
+        </div>
+      ) : (
+        <p onClick={() => setIsVisible(title)}>Show</p>
+      )}
+    </div>
+  );
+};
+const InstaMart = () => {
+  const [isVisible, setIsVisible] = useState("About");
+  return (
+    <div>
+      <Section
+        title={"About"}
+        desc={"description of about"}
+        isVisible={isVisible}
+        setIsVisible={setIsVisible}
+      ></Section>
+      <Section
+        title={"Team"}
+        desc={"description of Team"}
+        isVisible={isVisible}
+        setIsVisible={setIsVisible}
+      ></Section>
+      <Section
+        title={"Carrers"}
+        desc={"description of Carrers"}
+        isVisible={isVisible}
+        setIsVisible={setIsVisible}
+      ></Section>
+    </div>
+  );
+};
+export default InstaMart;
