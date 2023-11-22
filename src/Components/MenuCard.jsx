14ac86eeee84af56861fc1e9fc42350f9d8be3fc
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-11-22 19:46:04 +0530

    add remove separte button

--- src/Components/MenuCard.jsx
+++ src/Components/MenuCard.jsx
@@ -2,14 +2,10 @@ import { PiSquareLogoFill } from "react-icons/pi";
 import { MdOutlineCurrencyRupee } from "react-icons/md";
 import { MenuImgUrl } from "../constant";
 import { IoStar } from "react-icons/io5";
-import { useDispatch } from "react-redux";
-import { addItem } from "../Store/Slices/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, removeItem } from "../Store/Slices/cartSlice";
+
 const MenuCard = ({ menu }) => {
-
-  //despatch 
-  const dispatch = useDispatch();
-
-  
   return (
     <div className="text-lg font-semibold flex justify-between items-center gap-1 pt-5 pb-10 border-b">
       <div className="flex flex-col gap-2">
@@ -17,18 +13,17 @@ const MenuCard = ({ menu }) => {
           <span>
             {menu?.card?.info?.itemAttribute?.vegClassifier === "VEG" ? (
               <PiSquareLogoFill className="text-green-500 text-base " />
-              ) : (
+            ) : (
               <PiSquareLogoFill className="text-red-500 text-lg font-bold" />
             )}
-
           </span>
-          {menu?.card?.info?.isBestseller && <span className="flex gap-2 items-center text-base text-orange-500">
-            <IoStar/>
-            <p className="cursor-text">Bestseller</p>
-          </span>}
-                
+          {menu?.card?.info?.isBestseller && (
+            <span className="flex gap-2 items-center text-base text-orange-500">
+              <IoStar />
+              <p className="cursor-text">Bestseller</p>
+            </span>
+          )}
         </div>
-        
 
         <p className="text-base cursor-text">{menu?.card?.info?.name} </p>
 
@@ -41,39 +36,68 @@ const MenuCard = ({ menu }) => {
         </p>
       </div>
       <div>
-        {menu?.card?.info?.imageId ? (
-          <div className="relative">
-            <div className="w-[118px] h-[96px] rounded-md">
+        <div className="relative">
+          <div className="w-[118px] h-[96px] rounded-md">
+            {menu?.card?.info?.imageId && (
               <img
                 src={MenuImgUrl + menu?.card?.info?.imageId}
                 alt=""
-                className="w-full h-full rounded-md"/>
-
-                <AddButton info={menu?.card?.info}/>
-            </div>
+                className="w-full h-full rounded-md"
+              />
+            )}
+            <Button item={menu?.card?.info} />
           </div>
-        ) : (
-            <AddButton info={menu?.card?.info}/>
-        )}
+        </div>
       </div>
     </div>
   );
 };
 
-const AddButton = ({info}) => {
+const Button = ({ item }) => {
+  const cartItem = useSelector((store) => store.cart.items);
+
   const dispatch = useDispatch();
 
-  function handleAdd(info){
-    console.log("added - ", info)
-    dispatch(addItem(info));
-  }
-  return (
-    <div className={`${info.imageId && "absolute -bottom-2 left-2 bg-white"}  w-24 h-9 border border-gray-400 hover:drop-shadow-md drop-shadow-sm text-center rounded-md flex justify-center items-center`} onClick={() => handleAdd(info)}>
-            <p className="text-green-500 text-center align-middle font-semibold text-lg tracking-wide cursor-pointer" >
-              ADD
-            </p>
-      </div>
-  )
-}
+  const AddItem = () => {
+    dispatch(addItem(item))
+    console.log("ADDED : ", cartItem, typeof(cartItem));
+  };
 
+  const RemoveItem = () => {
+    dispatch(removeItem(item?.id));
+    console.log("RemoveItem : ",item?.id)
+  };
+  return (
+    <>
+      {cartItem.includes(item) ? (
+        <div
+          className={`${
+            item?.imageId && "absolute -bottom-3 left-2 bg-white text-green-500"
+          } flex justify-evenly w-10/12 border border-gray-300 bg-white text-green-600 rounded-md py-1`}
+        >
+          {" "}
+          <div onClick={RemoveItem} className="mx-2">
+            {" "}
+            -{" "}
+          </div>{" "}
+          <div> {cartItem?.length || 100} </div>{" "}
+          <div onClick={AddItem} className="mx-2">
+            {" "}
+            +{" "}
+          </div>{" "}
+        </div>
+      ) : (
+        <div
+          onClick={AddItem}
+          className={`${
+            item?.imageId && "absolute -bottom-3 left-2 bg-white text-green-500"
+          } flex justify-evenly w-10/12 border border-gray-300 bg-white text-green-600 rounded-md py-1 text-base font-mono tracking-wide`}
+        >
+          {" "}
+          ADD{" "}
+        </div>
+      )}
+    </>
+  );
+};
 export default MenuCard;
