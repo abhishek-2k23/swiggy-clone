1813165dd9d298ec00ab88fd6ccc395fb00577bf
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-01-20 04:06:25 +0530

    added add and remove function with item count

--- src/Store/Slices/cartSlice.js
+++ src/Store/Slices/cartSlice.js
@@ -6,8 +6,35 @@ const cartSlice = createSlice({
         items : [],
     },
     reducers : {
-    addItem : (state,action) => {return {...state,items : [...state.items,action.payload]}},
-    removeItem : (state, action) => { state.items = state.items.filter((item) => item.id == action.payload)},
+    addItem : (state,action) => {
+        console.log(action.payload);
+        if(state.items.find((item) => item.id === action.payload.id)){
+            console.log("Already exist, so inc");
+            const idx = state.items.findIndex((item) => item.id === action.payload.id);
+            
+            //update the no. of Items
+            state.items[idx].NoOfItems++;
+        }else{
+            const item = {
+                NoOfItems : action.payload.NoOfItems,
+                id : action.payload.id,
+                name : action.payload.name,
+                price : action.payload.price || 100
+            }
+            state.items.push(item);
+        }
+    },
+    removeItem : (state, action) => { 
+        const idx = state.items.findIndex((item) => item.id === action.payload);
+        let item = state.items[idx];
+        console.log(item);
+        if(item.NoOfItems > 1){
+            item.NoOfItems = item.NoOfItems - 1;
+            // state.items.push(item);
+        }else{
+            state.items = state.items.filter((item) => item.id !== action.payload);
+        }
+    },
     },
 })
 
