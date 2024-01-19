940fcff704c815dd93f1f8baa6be8dbf38ab353f
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-01-20 04:05:10 +0530

    added store item details

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,11 +1,24 @@
 import AddRemoveButton from "./AddRemoveButton";
-
+import { MdOutlineCurrencyRupee } from "react-icons/md";
+import { useSelector } from "react-redux";
+// import { addItem, removeItem } from "../Store/Slices/cartSlice";
+// import {Button} from "./MenuCard";
 const CartItem = ({item}) => {
+    //find the items count
+    const cartItems = useSelector(store => store.cart.items);
+    const idx = cartItems.findIndex(cartItem => cartItem.id === item.id);
+    const count = cartItems[idx].NoOfItems;
     return (
         <div className="flex w-full p-5 justify-between">
-            <p>{item.name}</p>
-            <AddRemoveButton item={item}/>
-            <p>{item.price/100}</p>
+            <div className="w-1/2 flex flex-wrap items-center justify-start">
+                <p>{item.name}</p>
+            </div>
+            <div className="flex justify-center items-center">
+                <AddRemoveButton item={item}/>
+            </div>
+            <div className="flex items-center">
+                <p className="flex items-center"> <MdOutlineCurrencyRupee /> {(item.price/100)*count}</p>
+            </div>
         </div>
     )
 }
